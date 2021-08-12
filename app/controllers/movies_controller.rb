c24d3aaf005f4274827c59e841afbb5c278bc66f
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-08-12 09:19:21 -0600

    commit tree ee91b4d6d293a13eb31659e98d42affd2eea42c1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,25 @@
 class MoviesController < ApplicationController
+  wrap_parameters format: []
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unaccesable_entity_response
   
   def index
     movies = Movie.all
     render json: movies
+  end
+
+  def create
+    movie = Movie.create!(movie_params)
+    render json: movie, status: :created
+  end
+
+  private
+
+  def movie_params
+    params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+  end
+
+  def render_unaccesable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unaccessable_entity
   end
 
 end
